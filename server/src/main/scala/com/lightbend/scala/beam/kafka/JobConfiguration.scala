b052b6113d9d0efb70c7cea2b63ee3e64ab5f00e
blublinsky <boris.lublinsky@lightbend.com>
2017-12-20 20:10:53 -0600

    Small cleanup

--- server/src/main/scala/com/lightbend/scala/beam/kafka/JobConfiguration.scala
+++ server/src/main/scala/com/lightbend/scala/beam/kafka/JobConfiguration.scala
@@ -1,10 +1,9 @@
 package com.lightbend.scala.beam.kafka
 
 import java.util
+
 import org.apache.beam.runners.flink.FlinkRunner
 import org.apache.beam.sdk.options.PipelineOptionsFactory
-import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.clients.producer.ProducerConfig
 
 object JobConfiguration {
   /**
@@ -14,26 +13,32 @@ object JobConfiguration {
     * @return Kafka props
     */
     def getKafkaSenderProps(options: KafkaOptions): util.Map[String, AnyRef] = {
+
+      import org.apache.kafka.clients.producer.ProducerConfig._
+
       val props: util.Map[String, AnyRef] = new util.HashMap[String, AnyRef]
-      props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, options.getBroker)
-      props.put(ProducerConfig.ACKS_CONFIG, "all")
-      props.put(ProducerConfig.RETRIES_CONFIG, "2")
-      props.put(ProducerConfig.BATCH_SIZE_CONFIG, "1024")
-      props.put(ProducerConfig.LINGER_MS_CONFIG, "1")
-      props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, "1024000")
+      props.put(BOOTSTRAP_SERVERS_CONFIG, options.getBroker)
+      props.put(ACKS_CONFIG, "all")
+      props.put(RETRIES_CONFIG, "2")
+      props.put(BATCH_SIZE_CONFIG, "1024")
+      props.put(LINGER_MS_CONFIG, "1")
+      props.put(BUFFER_MEMORY_CONFIG, "1024000")
       props
     }
 
   def getKafkaConsumerProps(options: KafkaOptions, data: Boolean): util.Map[String, AnyRef] = {
+
+    import org.apache.kafka.clients.consumer.ConsumerConfig._
+
     val props: util.Map[String, AnyRef] = new util.HashMap[String, AnyRef]
-    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, options.getBroker)
-    if (data) props.put(ConsumerConfig.GROUP_ID_CONFIG, options.getDataGroup)
-    else props.put(ConsumerConfig.GROUP_ID_CONFIG, options.getModelsGroup)
-    props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
-    props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
-    props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000")
-    props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "10000")
-    props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "10")
+    props.put(BOOTSTRAP_SERVERS_CONFIG, options.getBroker)
+    if (data) props.put(GROUP_ID_CONFIG, options.getDataGroup)
+    else props.put(GROUP_ID_CONFIG, options.getModelsGroup)
+    props.put(AUTO_OFFSET_RESET_CONFIG, "earliest")
+    props.put(ENABLE_AUTO_COMMIT_CONFIG, "true")
+    props.put(AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000")
+    props.put(SESSION_TIMEOUT_MS_CONFIG, "10000")
+    props.put(MAX_POLL_RECORDS_CONFIG, "10")
     props
   }
 
--- server/src/main/scala/com/lightbend/scala/beam/kafka/KafkaOptions.scala
+++ server/src/main/scala/com/lightbend/scala/beam/kafka/KafkaOptions.scala
@@ -1,39 +1,40 @@
 package com.lightbend.scala.beam.kafka
 
-import com.lightbend.configuration.ApplicationKafkaParameters
+import com.lightbend.configuration.ApplicationKafkaParameters._
 import org.apache.beam.runners.flink.FlinkPipelineOptions
 import org.apache.beam.sdk.options.Default
 import org.apache.beam.sdk.options.Description
 
 trait KafkaOptions extends FlinkPipelineOptions {
 
+
   @Description("The Kafka data topic to read data from")
-  @Default.String(ApplicationKafkaParameters.DATA_TOPIC) def getKafkaDataTopic: String
+  @Default.String(DATA_TOPIC) def getKafkaDataTopic: String
 
   def setKafkaDataTopic(value: String): Unit
 
   @Description("The Kafka topic to read models from")
-  @Default.String(ApplicationKafkaParameters.MODELS_TOPIC) def getKafkaModelsTopic: String
+  @Default.String(MODELS_TOPIC) def getKafkaModelsTopic: String
 
   def setKafkaModelsTopic(value: String): Unit
 
   @Description("The Kafka Broker to read from")
-  @Default.String(ApplicationKafkaParameters.LOCAL_KAFKA_BROKER) def getBroker: String
+  @Default.String(LOCAL_KAFKA_BROKER) def getBroker: String
 
   def setBroker(value: String): Unit
 
   @Description("The Zookeeper server to connect to")
-  @Default.String(ApplicationKafkaParameters.LOCAL_ZOOKEEPER_HOST) def getZookeeper: String
+  @Default.String(LOCAL_ZOOKEEPER_HOST) def getZookeeper: String
 
   def setZookeeper(value: String): Unit
 
   @Description("The Data Reading groupId")
-  @Default.String(ApplicationKafkaParameters.DATA_GROUP) def getDataGroup: String
+  @Default.String(DATA_GROUP) def getDataGroup: String
 
   def setDataGroup(value: String): Unit
 
   @Description("The Models Reading groupId")
-  @Default.String(ApplicationKafkaParameters.MODELS_GROUP) def getModelsGroup: String
+  @Default.String(MODELS_GROUP) def getModelsGroup: String
 
   def setModelsGroup(value: String): Unit
 }
